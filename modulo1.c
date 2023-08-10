80ce7d1de50e15c29bc1406b29a548dbde780283
iDeadtroll <javielproductor@gmail.com>
2023-08-10 16:18:16 +0200

    modulo1 +funcion para escribir un fichero

--- modulo1.c
+++ modulo1.c
@@ -1,34 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void write_le_fichier(char *phrase)
+
+
+void write_le_fichier(char *phrase, char *nom_fichier)
 {
-//     FILE *archivox;
+    FILE *archivox;
     
-//     archivox = fopen("archivoX.txt", "r");
-// // Si le fichier n'existe pas, il est créé et écrit dedans
-//     if (archivox == NULL) {
-//         archivox = fopen("archivoX.txt","w");
-//         fprintf(archivox,"%s", "hola_mundo");
-//         fclose(archivox);
-// // Si le fichier existe déjà, il est ouvert en mode 'append' et écrit dedans
-//     } else {
-//         archivox = fopen("archivoX.txt","a");
-//         fprintf(archivox, "%s","Hola Mundo");
-//         fclose(archivox);
-//     }
-    printf("%s\n",phrase);
+    archivox = fopen(nom_fichier, "r");
+// Si le fichier n'existe pas, il est créé et écrit dedans
+    if (archivox == NULL) {
+        archivox = fopen(nom_fichier,"w");
+        fprintf(archivox,"%s", phrase);
+        fclose(archivox);
+// Si le fichier existe déjà, il est ouvert en mode 'append' et écrit dedans
+    } else {
+        archivox = fopen(nom_fichier,"a");
+        fprintf(archivox, "%s",phrase);
+        fclose(archivox);
+    }
+}
+
+void introdiure_expression()
+{
+    char phrase[100];
+    char nom_fichier[100];
+    printf("Ingrese la palabra o frase: ");
+    fgets(phrase,sizeof(phrase),stdin);
+    printf("Introduce nombre del archivo: ");
+    // fgets(nom_fichier,sizeof(nom_fichier),stdin);
+
+    // // Eliminar el salto de línea del nombre del archivo
+    // nom_fichier[strcspn(nom_fichier, "\n")] = '\0';
+
+    scanf(" %[^\n]s", nom_fichier);
+    write_le_fichier(phrase,nom_fichier);
+
 }
 
 int main()
 {
-    char phrase[100];
-    // char chemin[100];
-    printf("Ingrese la palabra o frase: ");
-    fgets(phrase,sizeof(phrase),stdin);
-    // printf("Indica el nombre del archivo: ");
-    // fgets(chemin,sizeof(chemin),stdin);
-    write_le_fichier(phrase);
+    introdiure_expression();
     
     return 0;
 }
