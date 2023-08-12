e434a46a04ea5b7ab716e8652ca97f29700d9afb
iDeadtroll <javielproductor@gmail.com>
2023-08-12 06:18:52 +0200

    Añadido modulo 'main.c'

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "modulo1.c"
+#include <stdio.h>
+
+int main() {
+    char *chemin = "C:\\Users\\joni-\\Documents\\GitHub\\C_1";
+    
+    mostrarArchivosSinExtension(chemin);
+    
+    return 0;
+}
